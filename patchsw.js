24d91bcf4fc4de2aca0658f0dd165bd0ae24b919
Nicolas Pernoud <npernoud@grandlyon.com>
2018-08-09 15:30:02 +0200

    Corrected service worker patch

--- patchsw.js
+++ patchsw.js
@@ -4,8 +4,7 @@ fs.readFile('./dist/ngsw-worker.js', 'utf-8', (err, data) => {
     if (err) throw err;
     fs.writeFile('./dist/ngsw-worker.js', data.replace('onFetch(event) {', `
     onFetch(event) {
-        if (event.request.url.indexOf('/upload') !== -1) { return; }
-        if (event.request.url.indexOf('/share') !== -1) { return; }
+        if (event.request.url.indexOf('/api/files') !== -1) { return; }
         `), 'utf-8', (err) => {
             if (err) throw err;
             console.log('Service Worker patched !');
