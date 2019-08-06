94bf997d33385e51b63edc0e1fcd5e64c7d4c9be
Nicolas PERNOUD <github@ninico.fr>
2019-08-06 21:48:41 +0200

    fix: better animations fix

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, transition, style, animate, query } from '@angular/animations';
+import { trigger, transition, style, animate, query, group } from '@angular/animations';
 
 export const appAnimations = [
     trigger(
@@ -18,18 +18,16 @@ export const appAnimations = [
         ]
     ),
     trigger('routeAnimations', [
-        transition('login => *, apps => users, apps => explorer, explorer => apps, explorer => users, users => explorer, users => apps', [
-            /* REPLACE PREVIOUS LINE WITH * => * WHEN THE FIREFOX BUG WILL BE CORRECTED .... */
-            /*group([  // block executes in parallel
+        transition('* => *', [
+            group([  // block executes in parallel
                 query(':enter', [
                     style({ transform: 'scale(0)', opacity: 0 }),
-                    animate('0.3s ease-in-out', style({ transform: 'scale(1)', opacity: 1 }))
-                ], { optional: true }),/* ----- DEACTIVATED TEMPORARYLY TO MITIGATE A BUG WITH FIREFOX --------- */
+                    animate('0.3s ease-in-out')
+                ], { optional: true }),
                 query(':leave', [
-                    style({ transform: 'scale(1)', opacity: 1 }),
                     animate('0.3s ease-in-out', style({ transform: 'scale(0)', opacity: 0 }))
                 ], { optional: true }),
-            //])
+            ])
         ])
     ])
 ];
